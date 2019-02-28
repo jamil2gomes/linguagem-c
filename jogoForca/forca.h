73297a9f33d410531b9dabdef88ae2196233fa7e
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-28 17:25:22 -0300

    jogo da forca concluído

---
+++ jogoForca/forca.h
@@ -0,0 +1,23 @@
+
+
+#define TAMANHO_PALAVRA 20
+
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+int chuteserrados();
+void desenhacaveira();
+void desenhatrofeu();
+
+
+
+
+
+
+
+
+
